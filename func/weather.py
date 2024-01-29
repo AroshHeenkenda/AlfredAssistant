d6b25285d4175a3455a07fbb53fafbeeae196194
Arosh Heenkenda <ahee0011@student.monash.edu>
2024-01-30 10:44:21 +1100

    Testing out the weather API, seems to work nicely

--- func/weather.py
+++ func/weather.py
@@ -1,16 +1,37 @@
+from __future__ import print_function
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 KEY = os.getenv("WEATHER_KEY")
-BASE =  "http://api.weatherapi.com/v1" #base API url
 
 
 class Weather:
 
     def __init__(self) -> None:
-        pass
+        #Configure the API key
+        self.configuration = swagger_client.Configuration()
+        self.configuration.api_key['key'] = KEY
 
-    def current_weather():
-        request = BASE + f"/current.json?key={KEY}&q={'melbourne'}"
+        # create an instance of the API class
+        self.api_instance = swagger_client.APIsApi(swagger_client.ApiClient(self.configuration))
+
+    def current_weather(self):
+        
+        try:
+            api_resp = self.api_instance.realtime_weather("melbourne")
+            pprint(api_resp)
+        except ApiException as e:
+            print("Exception when calling APIsApi->realtime_weather: %s\n" % e)
+
+
+
+if __name__ == "__main__":
+
+    weather = Weather()
+    weather.current_weather()
